3bcdd91632ee8ab00b3c3bdec2bef6bff5f985e9
Szymon <cnk.one@gmail.com>
2017-06-22 02:32:58 +0200

    little fixes

--- map.php
+++ map.php
@@ -3,10 +3,9 @@
 <html lang="<?= $data['lang'] ?>">
 <head>
 	<meta charset="<?= $data['charset'] ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Google maps dojazd</title>
-	<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdn.rawgit.com/driftyco/ionicons/3.0/dist/css/ionicons.css">
 	<link rel="stylesheet" href="assets/css/gmRoute.css">
 </head>
@@ -14,47 +13,49 @@
 <div class="container-fluid">
 	<div class="row">
 		<h1>Google maps dojazd</h1>
+	</div>
+</div>
+<div class="container-fluid">
+	<div class="row">
 		<div id="map_canvas"></div>
 	</div>
-	<form onSubmit="calcRoute();return false;" id="routeForm">
-		<div class="row">
-			<div class="col-4 ml-auto">
-				<div class="row" style="overflow: hidden">
-					<div class="col-12">
-						<label>
-							Twoja lokalizacja: <br />
-							<input type="text" id="routeStart" value="Bielsko-Biała" placeholder="Wpisz nazwę miasta">
-						</label>
-					</div>
-					<div class="col-12">
-						<div class="h5">Środek transportu<!-- Travel mode -->:</div>
-							<label>
-								<input type="radio" name="travelMode" value="DRIVING" hidden checked />
-								<span class="icon-check icon-car" data-toggle="tooltip" title="Samochód"></span><!-- Driving -->
-							</label>
-							<label>
-								<input type="radio" name="travelMode" value="TRANSIT" hidden />
-								<span class="icon-check icon-transit" data-toggle="tooltip" title="Transport publiczny"></span><!-- Public transport -->
-							</label>
-							<label>
-								<input type="radio" name="travelMode" value="BICYCLING" hidden />
-								<span class="icon-check icon-bike" data-toggle="tooltip" title="Rower"></span><!--Bicylcing -->
-							</label>
-							<label>
-								<input type="radio" name="travelMode" value="WALKING" hidden />
-								<span class="icon-check icon-walking" tdata-toggle="tooltip" itle="Pieszo"></span><!-- Walking -->
-							</label>
-					</div>
-					<div class="col-12">
-						<button type="submit" class="btn btn-secondary">
-							<i class="icon ion-ios-search"></i>
-							Oblicz trase
-						</button><!-- Calculate route -->
+	<div class="gmRoute-form">
+		<form onSubmit="calcRoute();return false;" id="routeForm">
+			<div class="row">
+				<div class="col col-md-3 col-xl-3 col-xxl-2 ml-auto">
+					<div class="row">
+						<div class="col-12">
+							<div class="input-group">
+								<input type="text" id="routeStart" class="form-control" placeholder="Twoja lokalizacja...">
+								<span class="input-group-btn"><!-- Calculate route -->
+									<button class="btn btn-secondary" type="submit"><i class="icon ion-ios-search"></i></button>
+								</span>
+							</div>
+						</div>
+						<div class="col-12">
+							<div class="h5">Środek transportu<!-- Travel mode -->:</div>
+								<label>
+									<input type="radio" name="travelMode" value="DRIVING" hidden checked />
+									<span class="icon-travel-mode icon-check icon-car" data-toggle="tooltip" title="Samochód"></span><!-- Driving -->
+								</label>
+								<label>
+									<input type="radio" name="travelMode" value="TRANSIT" hidden />
+									<span class="icon-travel-mode icon-check icon-transit" data-toggle="tooltip" title="Transport publiczny"></span><!-- Public transport -->
+								</label>
+								<label>
+									<input type="radio" name="travelMode" value="BICYCLING" hidden />
+									<span class="icon-travel-mode icon-check icon-bike" data-toggle="tooltip" title="Rower"></span><!--Bicylcing -->
+								</label>
+								<label>
+									<input type="radio" name="travelMode" value="WALKING" hidden />
+									<span class="icon-travel-mode icon-check icon-walking" data-toggle="tooltip" title="Pieszo"></span><!-- Walking -->
+								</label>
+						</div><!-- Calculate route -->
 					</div>
 				</div>
 			</div>
-		</div>
-	</form>
+		</form>
+	</div>
 	<div id="directionsPanel">
 		s
 		<!-- Enter a destination and click "Calculate route". -->
